09b81b3a2300c57b0d366668419f4035c7bfeaaa
jspinerola <144980273+jspinerola@users.noreply.github.com>
2024-11-07 21:06:49 -0600

    erm

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,2 +1,87 @@
-package PACKAGE_NAME;public class HashTable {
+public class HashTable {
+    private Node[] table;
+    private int capacity;
+    private int occupants;
+    private final Node VACANT = new Node(-1, "N/A");
+
+    public HashTable(int capacity) {
+        this.capacity = capacity;
+        this.table = new Node[capacity];
+        this.occupants = 0;
+    }
+
+    public int hashCode(Integer key) {
+        return key % capacity;
+    }
+
+    public int doubleHash(Integer key, int i) {
+        return (hashCode(key) + Math.abs(13 - key % 13) * i) % capacity;
+    }
+
+    public String get(Integer key) {
+        int hash = hashCode(key);
+        int count = 1;
+
+        while(table[hash] != null && table[hash].getKey() != key) {
+            hash = doubleHash(key, count);
+            count++;
+        }
+
+        if (table[hash] == null) {
+            return null;
+        }
+
+        return table[hash].getData();
+    }
+
+    // Implement put method for in-class assignment
+    public void put(Node node) {
+        int collisionCount = 0;
+        int hash = hashCode(node.getKey());
+
+        while (table[hash] != null){
+            collisionCount++;
+            hash = doubleHash(node.getKey(), collisionCount);
+        }
+        table[hash] = node;
+    }
+
+    // Implement remove and rehash methods for lab assignment
+
+    public String toString() {
+        String s = "[";
+        String color;
+
+        for (int index = 0; index < this.capacity - 1; index++) {
+            color = table[index] == null ? null : table[index].getData();
+            s += color + ", ";
+
+            if (index % 10 == 9) {
+                s += "\n";
+            }
+        }
+
+        color = table[this.capacity - 1] == null ? null :
+                table[this.capacity - 1].getData();
+        s += color + "]";
+        return s;
+    }
+}
+
+class Node {
+    Integer key;
+    String data;
+
+    public Node(Integer key, String data) {
+        this.key = key;
+        this.data = data;
+    }
+
+    public Integer getKey() {
+        return key;
+    }
+
+    public String getData() {
+        return data;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        HashTable table = new HashTable(10);
+
+        // Insert several values, including some collisions
+        table.put(new Node(0, "red"));
+        table.put(new Node(1, "blue"));
+        table.put(new Node(2, "green"));
+        table.put(new Node(3, "yellow"));
+        table.put(new Node(10, "orange"));
+        table.put(new Node(9, "purple"));
+        table.put(new Node(19, "brown"));
+
+        // Remove a value to ensure proceeding accesses still work
+//        table.remove(0);
+
+        System.out.println(table);
+        System.out.println(table.get(9));
+        System.out.println(table.get(10));
+        System.out.println(table.get(11));
+
+        // Insert another value using the key from removal
+        table.put(new Node(0, "cyan"));
+
+        System.out.println(table);
+
+        // Insert more values, requiring a rehash
+        table.put(new Node(5, "magenta"));
+        table.put(new Node(6, "peach"));
+        table.put(new Node(7, "black"));
+        table.put(new Node(8, "white"));
+
+        System.out.println(table);
     }
 }
